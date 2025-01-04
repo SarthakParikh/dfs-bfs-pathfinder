71c0e5eafe3ddb4c37afc8a6eb90d8aca18d9092
Sarthak Parikh <sarthakparikh20010409@gmail.com>
2025-01-04 10:39:47 -0500

    Path Finder Project (DFS/BFS) Algo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { use, useEffect, useState } from "react";
+import MazeGrid from "./MazeGrid";
+
+
+function App() {
+   return <MazeGrid />
+}
+
+export default App;
